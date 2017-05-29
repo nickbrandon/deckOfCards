6eeed550d2d1f3ca261357e73d2db3c4f1cf64a8
Nick <Nick@administrators-MacBook-Pro-9.local>
2017-05-29 12:29:02 -0700

    refactor logic into container;  add knuth shuffle algorithm

--- src/component/CardPresenter.js
+++ src/component/CardPresenter.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { Card } from 'antd';
-import buildDeck from './buildDeck';
 
 const CardPresenter = (props) => {
-	let cards = buildDeck();
-
-	let deck = cards.map(function(card) {
-		return	<div>
-				<Card title="" >
-					<div style={ {height: 100} }>
-						<img height="100px" src={'.\\cards\\' + card}  />
-					</div>
-				</Card>
-				</div>
-	});
-
 	return (
 		<div className="cardList">
-			{deck}
+			{props.deck}
 		</div>
 		);
 };
--- src/component/cardContainer.js
+++ src/component/cardContainer.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import CardPresenter from './CardPresenter';
+import buildDeck from './buildDeck';
+import { Card } from 'antd';
+
+function knuthShuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+let cards = buildDeck();
+
+let deck = cards.map(function(card) {
+	return	<div>
+				<Card title="" >
+					<div style={ {height: 100} }>
+						<img height="100px" src={'.\\cards\\' + card}  />
+					</div>
+				</Card>
+			</div>
+	});
 
 const CardContainer = () => {
-	return <CardPresenter />
+	return (
+		<CardPresenter deck={deck}/>
+		);
 };
 
 export default CardContainer;
